3985b91d30dc3ac740ae0eebd49e64aab1890f23
mizu-awa <mizuawakg@gmail.com>
2020-07-22 23:22:17 +0900

    クッションバッチ更新

--- easy_update/bat/cushion.bat
+++ easy_update/bat/cushion.bat
@@ -10,21 +10,21 @@ if exist ..\..\substance (
 	rem substanceフォルダの中身をすべてrootにコピー
 	xcopy /e ..\..\substance ..\..\
 	rem substanceフォルダを消去
-	rmdir /s ..\..\substance
+	rmdir /s /q ..\..\substance
 ) else (
 	rem substanceフォルダが存在していない場合
 	echo クッションページを作成します。
 	rem substanceフォルダを作成する
 	mkdir ..\..\substance
-	rem rootの中身（substance robot.txt easy_update以外）をsubstanceフォルダにコピーする
-	xcopy /e ..\..\ ..\..\substance /EXCLUDE: ..\data\xcopy-excludelist.txt
-	rem 残ったファイル・フォルダを削除(ユーザが追加したフォルダがあった場合使えないが…)
-	rmdir /s ..\..\contents
-	rmdir /s ..\..\data
-	rmdir /s ..\..\img
-	rmdir /s ..\..\js
+	echo rootの中身（substance robot.txt easy_update以外）をsubstanceフォルダにコピーする
+	xcopy /e /EXCLUDE: ..\data\xcopy-excludelist.txt ..\..\ ..\..\substance
+	echo 残ったファイル・フォルダを削除(ユーザが追加したフォルダがあった場合使えないが…)
+	rmdir /s /q ..\..\contents
+	rmdir /s /q ..\..\data
+	rmdir /s /q ..\..\img
+	rmdir /s /q ..\..\js
 	del ..\..\index.html
 	del ..\..\style.css
-	rem index.htmlを作成
+	echo index.htmlを作成
 	copy ..\data\index.html ..\..\index.html
 )
